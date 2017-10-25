c54d2715a021f7c64fd333aeb816b73604aaedf5
hummaad <hummaad@live.com>
2017-10-25 17:45:07 -0400

    Website Split into conponents

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 import * as $ from 'jquery';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { UserMainComponent } from './components/user-main/user-main.component';
+import { AudioPlayerComponent } from './components/user-main/audio-player/audio-player.component';
+import { ContainerComponent } from './components/user-main/container/container.component';
+import { ContentContainerComponent } from './components/user-main/content-container/content-container.component';
+import { LeftNavComponent } from './components/user-main/left-nav/left-nav.component';
+import { RightNavComponent } from './components/user-main/right-nav/right-nav.component';
+import { TopMenuComponent } from './components/user-main/top-menu/top-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    UserMainComponent,
+    AudioPlayerComponent,
+    ContainerComponent,
+    ContentContainerComponent,
+    LeftNavComponent,
+    RightNavComponent,
+    TopMenuComponent,
+
 
   ],
   imports: [
