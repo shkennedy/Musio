f7abd80e7172649286bb8b4134bf5296b3997135
Pierre94 <bigchenzzy@gmail.com>
2017-11-02 01:39:37 -0400

    log out doesn't work makes no sense

--- webui/src/app/login/login.component.ts
+++ webui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
     //previous function
     var screenheight = window.innerHeight;
     var html = document.getElementsByTagName('html')[0];
     html.setAttribute("style", "--global-screen-height: " + (screenheight) + "px");
+  }
+
+  onLogIn() {
+    console.log("No DB exists. Log in accepted");
+    this.router.navigate(['/home']);
   }
 
 }
