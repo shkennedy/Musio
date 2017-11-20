3c3f428b8e8e17ca271ea352996d67c823646cdd
Pierre94 <bigchenzzy@gmail.com>
2017-11-19 22:48:23 -0500

    so much to do still

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule} from './app-routing.module'
 
 //import { AuthGuard } from './auth-guard.service'
@@ -57,9 +58,10 @@ import { UserService } from './services/user.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  //providers: [AuthGuard],
+  //services go here
   providers: [
     AlbumService,
     ArtistService,
