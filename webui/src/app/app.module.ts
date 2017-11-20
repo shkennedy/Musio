99305eb0d7b0aab9c0506b8090435cd41f199e66
Shane Kennedy <shanekennedy7135@gmail.com>
2017-11-19 22:48:31 -0500

    Added some semicolons

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
-import { AppRoutingModule} from './app-routing.module'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 //import { AuthGuard } from './auth-guard.service'
 
