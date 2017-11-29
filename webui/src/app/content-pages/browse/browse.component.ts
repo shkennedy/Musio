f58e0dac3b2dd59e3344880c3b902c3516ca8f7e
Shane Kennedy <shane.kennedy@stonybrook.edu>
2017-11-29 16:20:45 -0500

    Added browse to services and browse controller

--- webui/src/app/content-pages/browse/browse.component.ts
+++ webui/src/app/content-pages/browse/browse.component.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { Song } from '../../models/song.model'
-import { Album } from '../../models/album.model'
-import { Artist } from '../../models/artist.model'
-import { Playlist } from '../../models/playlist.model'
+import { Router } from '@angular/router';
+
+import { SearchService } from '../../services/search.service';
+import { BrowseResponse } from '../../services/search.service';
+
+import { Song } from '../../models/song.model';
+import { Album } from '../../models/album.model';
+import { Artist } from '../../models/artist.model';
+import { Genre } from '../../models/genre.model';
+import { Playlist } from '../../models/playlist.model';
+import { Instrument } from '../../models/instrument.model';
+
 @Component({
-  selector: 'app-browse',
-  templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+    selector: 'app-browse',
+    templateUrl: './browse.component.html',
+    styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
-  public albums:Album[];
-  public artists:Artist[];
-  public playlists:Playlist[];
-  public songs:Song[];
-  constructor() {}
+    
+    private albums: Album[];
+    private artists: Artist[];
+    private genres: Genre[];
+    private instruments: Instrument[];
+    private playlists: Playlist[];
+    private songs: Song[];
 
-  ngOnInit() {
-  }
+    private errorMessage: string;
+    
+    constructor(
+        private router: Router,
+        private searchService: SearchService
+    ) { }
 
+    ngOnInit() {
+        this.searchService.getBrowse()
+        .subscribe(
+            (browseData: BrowseResponse) => {
+                this.albums = browseData.albums;
+                this.artists = browseData.artists;
+                this.instruments = browseData.instruments
+                this.genres = browseData.genres;
+                this.playlists = browseData.playlists;
+                this.songs = browseData.songs;
+            },
+            (error: any) => {
+                this.errorMessage = error;
+                console.log(error);
+            }
+        );
+    }
 }
--- webui/src/app/services/search.service.ts
+++ webui/src/app/services/search.service.ts
@@ -7,6 +7,7 @@ import { HttpRequestService, ApiResponse } from './httpRequest.service';
 import { Album } from '../models/album.model';
 import { Artist } from '../models/artist.model';
 import { Genre } from '../models/genre.model';
+import { Instrument } from '../models/instrument.model';
 import { Playlist } from '../models/playlist.model';
 import { Song } from '../models/song.model';
 import { Station } from '../models/station.model';
@@ -17,13 +18,23 @@ export interface SearchResponse {
     genres: Genre[];
     playlists: Playlist[];
     songs: Song[];
-    stations: Station[];
+    // stations: Station[]; TODO add when exists on back end
+}
+
+export interface BrowseResponse {
+    songs: Song[];
+    artists: Artist[];
+    albums: Album[];
+    playlists: Playlist[];
+    instruments: Instrument[];
+	genres: Genre[];
 }
 
 @Injectable()
 export class SearchService {
 
     private static SEARCH_URL: string = '/search';
+    private static BROWSE_URL: string = '/browse';
 
     constructor(
         private router: Router,
@@ -36,4 +47,11 @@ export class SearchService {
     //         return response.responseData;
     //     });
     // }
+
+    public getBrowse(): Observable<BrowseResponse> {
+        return this.httpRequest.get(SearchService.BROWSE_URL)
+        .map((response: ApiResponse) => {
+            return response.responseData;
+        });
+    }
 }
