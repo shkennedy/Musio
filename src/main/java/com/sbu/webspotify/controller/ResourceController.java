5b16cb20a3cb4f1932e9b431a5035d00384bcdb5
SC <sclark364@gmail.com>
2017-11-30 17:49:08 -0500

    Added easy endpoints for getting song files.

--- src/main/java/com/sbu/webspotify/controller/ResourceController.java
+++ src/main/java/com/sbu/webspotify/controller/ResourceController.java
@@ -1,8 +1,11 @@
 package com.sbu.webspotify.controller;
 
 import com.sbu.webspotify.dto.ApiResponseObject;
+import com.sbu.webspotify.model.Audio;
 import com.sbu.webspotify.model.File;
+import com.sbu.webspotify.model.Song;
 import com.sbu.webspotify.repo.FileRepository;
+import com.sbu.webspotify.service.SongService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +21,9 @@ public class ResourceController
     @Autowired
     FileRepository fileRepository;
 
+    @Autowired
+    SongService songService;
+
     @RequestMapping(value = "/get/{fileId}", method = RequestMethod.GET)
     public @ResponseBody ApiResponseObject getResource(@PathVariable("fileId") int fileId) {
         ApiResponseObject response = new ApiResponseObject();
@@ -25,6 +31,64 @@ public class ResourceController
         if(file == null) {
             response.setSuccess(false);
             response.setMessage("No file found with id "+fileId+".");
+        }
+        else {
+            response.setSuccess(true);
+            response.setResponseData(file);
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/highBitrateSongFile/{songId}", method = RequestMethod.GET)
+    public @ResponseBody ApiResponseObject getHighBitrateSong(@PathVariable("songId") int songId) {
+        ApiResponseObject response = new ApiResponseObject();
+        Song song = songService.getSongById(songId);
+        if(song == null) {
+            response.setSuccess(false);
+            response.setMessage("No song found with id "+songId+".");
+            return response;
+        }
+
+        Audio audio = song.getAudio();
+        if(audio == null) {
+            response.setSuccess(false);
+            response.setMessage("Could not retrieve high bitrate data for song with id "+songId+".");
+            return response;
+        }
+
+        File file = fileRepository.findById(audio.getHighBitrateFileId());
+        if(file == null) {
+            response.setSuccess(false);
+            response.setMessage("Could not retrieve high bitrate data for song with id "+songId+".");
+        }
+        else {
+            response.setSuccess(true);
+            response.setResponseData(file);
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/lowBitrateSongFile/{songId}", method = RequestMethod.GET)
+    public @ResponseBody ApiResponseObject getLowBitrateSong(@PathVariable("songId") int songId) {
+        ApiResponseObject response = new ApiResponseObject();
+        Song song = songService.getSongById(songId);
+        if(song == null) {
+            response.setSuccess(false);
+            response.setMessage("No song found with id "+songId+".");
+            return response;
+        }
+
+        Audio audio = song.getAudio();
+        if(audio == null) {
+            response.setSuccess(false);
+            response.setMessage("Could not retrieve low bitrate data for song with id "+songId+".");
+            return response;
+        }
+
+        File file = fileRepository.findById(audio.getLowBitrateFileId());
+        if(file == null) {
+            response.setSuccess(false);
+            response.setMessage("Could not retrieve low bitrate data for song with id "+songId+".");
         }
         else {
             response.setSuccess(true);
