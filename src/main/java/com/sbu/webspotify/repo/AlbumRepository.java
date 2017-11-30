03b8b3cbe694048de4ab2273a9b8607d65cabeb1
SC <sclark364@gmail.com>
2017-11-29 19:42:02 -0500

    Updated browse response.

--- src/main/java/com/sbu/webspotify/repo/AlbumRepository.java
+++ src/main/java/com/sbu/webspotify/repo/AlbumRepository.java
@@ -19,6 +19,10 @@ public interface AlbumRepository extends JpaRepository<Album, Integer>
                     nativeQuery = true)
     Set<AlbumIdentifier> findByNameContaining(@Param("queryString") String queryString);
 
-    @Query(value = "SELECT a.id as id, a.title as title FROM user_favorite_album ufa, album a WHERE ufa.album_id = a.id and ufa.user_id = :userId ORDER BY timestamp DESC LIMIT :numElements", nativeQuery = true)    
+    @Query(value = "SELECT art.id as artistId, art.name as artistName, alb.id as id, alb.title as title "
+                    + "FROM user_favorite_album ufa, album alb, artist art, album_artist_mapping aam "
+                    + "WHERE ufa.album_id = alb.id AND ufa.user_id = :userId AND aam.artist_id = art.id AND aam.album_id = alb.id "
+                    + "ORDER BY timestamp DESC LIMIT :numElements",
+                    nativeQuery = true)    
 	Set<AlbumIdentifier> findRecentlyFavoritedAlbumsByUser(@Param("userId") int userId, @Param("numElements") int numElements);
 }
--- src/main/java/com/sbu/webspotify/repo/SongRepository.java
+++ src/main/java/com/sbu/webspotify/repo/SongRepository.java
@@ -14,11 +14,15 @@ public interface SongRepository extends JpaRepository<Song, Integer>
     Song findById(int id);
 
     @Query(value = "SELECT alb.id as id, alb.title as albumTitle, art.id as artistId, art.name as artistName, s.id as albumId, s.title as title "
-            + "FROM Song s, Album alb, Artist art, song_album_mapping sam, album_artist_mapping aam "
-            + "WHERE s.id = sam.song_id AND sam.album_id = alb.id AND aam.artist_id = art.id AND aam.album_id = alb.id AND s.title LIKE CONCAT('%', :queryString, '%')",
-            nativeQuery = true)
+                    + "FROM Song s, Album alb, Artist art, song_album_mapping sam, album_artist_mapping aam "
+                    + "WHERE s.id = sam.song_id AND sam.album_id = alb.id AND aam.artist_id = art.id AND aam.album_id = alb.id AND s.title LIKE CONCAT('%', :queryString, '%')",
+                    nativeQuery = true)
     Set<SongIdentifier> findByNameContaining(@Param("queryString") String queryString);
 
-    @Query(value = "SELECT s.id as id, s.title as title FROM user_favorite_song ufs, song s WHERE ufs.song_id = s.id and ufs.user_id = :userId ORDER BY timestamp DESC LIMIT :numElements", nativeQuery = true)    
+    @Query(value = "SELECT alb.id as id, alb.title as albumTitle, art.id as artistId, art.name as artistName, s.id as albumId, s.title as title "
+                    + "FROM user_favorite_song ufs, Song s, Album alb, Artist art, song_album_mapping sam, album_artist_mapping aam "
+                    + "WHERE ufs.song_id = s.id and ufs.user_id = :userId AND s.id = sam.song_id AND sam.album_id = alb.id AND aam.artist_id = art.id AND aam.album_id = alb.id "
+                    + "ORDER BY timestamp DESC LIMIT :numElements",
+                    nativeQuery = true)    
 	Set<SongIdentifier> findRecentlyFavoritedSongsByUser(@Param("userId") int userId, @Param("numElements") int numElements);
 }
