3dacef64eb98694feac0ad9c033a34483dd47bb7
Shane <shane.kennedy@stonybrook.edu>
2017-11-30 22:36:51 -0500

    Pre switch to AmplitudeJS

--- webui/src/app/content-home/audio-player/audio-player.component.ts
+++ webui/src/app/content-home/audio-player/audio-player.component.ts
@@ -7,117 +7,117 @@ import { Song } from '../../models/song.model';
 import { SongService } from '../../services/song.service';
 
 @Component({
-  selector: 'app-audio-player',
-  templateUrl: './audio-player.component.html',
-  styleUrls: ['./audio-player.component.css'],
-  providers: [SongService]
+    selector: 'app-audio-player',
+    templateUrl: './audio-player.component.html',
+    styleUrls: ['./audio-player.component.css'],
+    providers: [SongService]
 })
 export class AudioPlayerComponent implements OnInit {
 
-  private static IN_PROGRESS_TIME = 5;
+    private static IN_PROGRESS_TIME = 5;
 
-  // Main
-  // https://github.com/goldfire/howler.js#documentation
+    // Main
+    // https://github.com/goldfire/howler.js#documentation
 
-  // Alternatives
-  // https://wiki.mozilla.org/Audio_Data_API#API_Tutorial
-  // https://github.com/arielfaur/ionic-audio/tree/3.0
+    // Alternatives
+    // https://wiki.mozilla.org/Audio_Data_API#API_Tutorial
+    // https://github.com/arielfaur/ionic-audio/tree/3.0
 
-  model: {
-    currentSong: Song,
-    songQueue: Song[],
-    history: Song[],
-    volume: number,
-    isMuted: boolean,
-    isPlaying: boolean,
-    isRepeating: boolean,
-  };
-  errMsg: string;
+    model: {
+        currentSong: Song,
+        songQueue: Song[],
+        history: Song[],
+        volume: number,
+        isMuted: boolean,
+        isPlaying: boolean,
+        isRepeating: boolean,
+    };
+    errMsg: string;
 
-  private audio: Howl;
+    private audio: Howl;
 
-  constructor(
-    private router: Router,
-    private songService: SongService
-  ) { }
+    constructor(
+        private router: Router,
+        private songService: SongService
+    ) { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
-//   private makeHowl(song: Song): Howl {
-//     const newHowl: Howl = new Howl({
-//       src: [song.audio],
-//       volume: this.model.volume,
-//       autoplay: true,
-//       onend: function () {
-//         this.forward();
-//       },
-//       onloaderror: function () {
-//         this.errMsg = 'Unable to load song';
-//       },
-//       onplayerror: function () {
-//         this.errMsg = 'Unable to play song';
-//       }
-//     });
-//   }
-//
-//   public playSong(songId: number): void {
-//     this.songService.getSongById(4)
-//     .subscribe((song: Song) => {
-//       if (!song) {
-//         return;
-//       }
-//       this.audio = this.makeHowl(song);
-//       this.model.currentSong = song;
-//     });
-//   }
-//
-//   public playNext(): void {
-//     const nextSong: Song = this.model.songQueue.shift();
-//     this.model.history.push(this.model.currentSong);
-//     if (nextSong) {
-//       this.playSong(nextSong.id);
-//     }
-//   }
-//
-//   public playLast(): void {
-//     // If current song is in progress, restart
-//     if (this.audio.seek() > AudioPlayerComponent.IN_PROGRESS_TIME) {
-//       this.audio.seek(0);
-//       return;
-//     }
-//
-//     // Find last song
-//     const lastSong: Song = this.model.history.pop();
-//
-//     // Add current song to songQueue
-//     this.model.songQueue.push(this.model.songQueue[0]);
-//     this.model.songQueue[0] = this.model.currentSong;
-//
-//     if (lastSong) {
-//       this.playSong(lastSong.id);
-//     }
-//   }
-//
-//   public playOrPause(): void {
-//     if (!this.model.currentSong) {
-//       return;
-//     }
-//
-//     if (this.model.isPlaying) {
-//       this.audio.pause();
-//     } else {
-//       this.audio.play();
-//     }
-//     this.model.isPlaying = !this.model.isPlaying;
-//   }
-//
-//   public muteOrUnmute(): void {
-//     this.model.isMuted = !this.model.isMuted;
-//     this.audio.mute(this.model.isMuted);
-//   }
-//
-//   public repeat(): void {
-//     this.model.isRepeating = !this.model.isRepeating;
-//     this.audio.loop(this.model.isRepeating);
-//   }
+    private makeHowl(song: Song): Howl {
+        const newHowl: Howl = new Howl({
+            src: [song.audio],
+            volume: this.model.volume,
+            autoplay: true,
+            onend: function () {
+                this.forward();
+            },
+            onloaderror: function () {
+                this.errMsg = 'Unable to load song';
+            },
+            onplayerror: function () {
+                this.errMsg = 'Unable to play song';
+            }
+        });
+    }
+
+    public playSong(songId: number): void {
+        this.songService.getSongById(4)
+            .subscribe((song: Song) => {
+                if (!song) {
+                    return;
+                }
+                this.audio = this.makeHowl(song);
+                this.model.currentSong = song;
+            });
+    }
+
+    public playNext(): void {
+        const nextSong: Song = this.model.songQueue.shift();
+        this.model.history.push(this.model.currentSong);
+        if (nextSong) {
+            this.playSong(nextSong.id);
+        }
+    }
+
+    public playLast(): void {
+        // If current song is in progress, restart
+        if (this.audio.seek() > AudioPlayerComponent.IN_PROGRESS_TIME) {
+            this.audio.seek(0);
+            return;
+        }
+
+        // Find last song
+        const lastSong: Song = this.model.history.pop();
+
+        // Add current song to songQueue
+        this.model.songQueue.push(this.model.songQueue[0]);
+        this.model.songQueue[0] = this.model.currentSong;
+
+        if (lastSong) {
+            this.playSong(lastSong.id);
+        }
+    }
+
+    public playOrPause(): void {
+        if (!this.model.currentSong) {
+            return;
+        }
+
+        if (this.model.isPlaying) {
+            this.audio.pause();
+        } else {
+            this.audio.play();
+        }
+        this.model.isPlaying = !this.model.isPlaying;
+    }
+
+    public muteOrUnmute(): void {
+        this.model.isMuted = !this.model.isMuted;
+        this.audio.mute(this.model.isMuted);
+    }
+
+    public repeat(): void {
+        this.model.isRepeating = !this.model.isRepeating;
+        this.audio.loop(this.model.isRepeating);
+    }
 }
--- webui/src/app/models/song.model.ts
+++ webui/src/app/models/song.model.ts
@@ -2,10 +2,12 @@ import { Artist } from './artist.model';
 import { Genre } from './genre.model';
 
 export class Song {
-  public id: number;
-  public title: string;
-  public duration: string;
-  public lyrics: string;
-  public mbid: string;
-  public artist: Artist;
+    public id: number;
+    public title: string;
+    public duration: string;
+    public lyrics: string;
+    public mbid: string;
+    public artist: Artist;
+
+    public audio: string;
 }
