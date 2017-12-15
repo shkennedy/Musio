1eeed2bef1026610969f263bfc9e17fb6ae4dfd1
Shane <shanekennedy7135@gmail.com>
2017-12-15 15:48:25 -0500

    Favorite songs view

--- webui/src/app/content-pages/song-view/song-view.component.ts
+++ webui/src/app/content-pages/song-view/song-view.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatTableDataSource, MatRow, MatSort } from '@angular/material';
 
+import { AudioPlayerProxyService } from '../../services/audioPlayerProxy.service';
 import { SongService } from '../../services/song.service';
 import { FavoritesService } from '../../services/favorites.service';
 
@@ -14,10 +16,14 @@ import { Artist } from '../../models/artist.model';
 })
 export class SongViewComponent implements OnInit {
 
-    private songs: Song[];
+    private songs: Map<number, Song>;
+    @ViewChild(MatSort) sort: any;
+    private songTableData: MatTableDataSource<Song>;
+    private playButtonsVisibility: Map<number, boolean> = new Map();
 
     constructor(
         private router: Router,
+        private audioPlayerProxyService: AudioPlayerProxyService,
         private songService: SongService,
         private favoritesService: FavoritesService
     ) { }
@@ -28,11 +34,69 @@ export class SongViewComponent implements OnInit {
             .subscribe(
             (songs: Song[]) => {
                 if (songs.length !== 0) {
-                    this.songs = songs;
+                    this.songTableData = new MatTableDataSource(songs);
+                    this.songs = new Map();
+                    songs.forEach((song: Song) => {
+                        this.songs.set(song.id, song);
+                        this.playButtonsVisibility.set(song.id, false);
+
+                        song.isFavorited = true;
+                        song.duration = Math.floor(song.duration / 1000);
+                        song.durationString =
+                            `${Math.floor(song.duration / 60)}:`;
+                        const seconds = song.duration % 60;
+                        song.durationString += (seconds < 10) ? `0${seconds}` : seconds;
+                    });
+
+                    setTimeout(() => {
+                        this.songTableData.sort = this.sort;
+                    }, 2000);
                 }
             },
             (error: any) => {
                 console.log(error.toString());
             });
+    }
+
+    private playSong(songId: number): void {
+        this.audioPlayerProxyService.playSong(songId);
+    }
+
+    private addSongToQueue(songId: number): void {
+        this.audioPlayerProxyService.addSongToQueue(songId);
+    }
+
+    private favoriteOrUnfavoriteSong(songId: number): void {
+        if (this.songs.get(songId).isFavorited) {
+            this.favoritesService.removeFavoriteSongById(songId)
+                .subscribe((success: boolean) => {
+                    this.songs.get(songId).isFavorited = !success;
+                });
+        } else {
+            this.favoritesService.addFavoriteSongById(songId)
+                .subscribe((success: boolean) => {
+                    this.songs.get(songId).isFavorited = success;
+                });
+        }
+    }
+
+    private getFavoritedIcon(songId: number, isFavorited: boolean): Object {
+        if (this.playButtonsVisibility.get(songId)) {
+            return (isFavorited) ? 'remove' : 'add';
+        } else {
+            return (isFavorited) ? 'favorite' : 'add';
+        }
+    }
+
+    private showPlay(songId: number): void {
+        this.playButtonsVisibility.set(songId, true);
+    }
+
+    private hidePlay(songId: number): void {
+        this.playButtonsVisibility.set(songId, false);
+    }
+
+    private getPlayVisibility(songId: number): Object {
+        return { 'visibility': this.playButtonsVisibility.get(songId) ? 'visible' : 'hidden' };
     }
 }
