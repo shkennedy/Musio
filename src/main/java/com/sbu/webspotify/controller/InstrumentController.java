b9a4a2d82301f1a8383208193bb32b44f47cf7c5
Shane <shanekennedy7135@gmail.com>
2017-12-17 17:17:49 -0500

    Added findall for instrument

--- src/main/java/com/sbu/webspotify/controller/InstrumentController.java
+++ src/main/java/com/sbu/webspotify/controller/InstrumentController.java
@@ -3,10 +3,15 @@ package com.sbu.webspotify.controller;
 import com.sbu.webspotify.dto.ApiResponseObject;
 import com.sbu.webspotify.dto.SearchResults;
 import com.sbu.webspotify.repo.InstrumentRepository;
+import com.sbu.webspotify.service.InstrumentService;
 import com.sbu.webspotify.service.SearchService;
+import com.sbu.webspotify.model.Instrument;
+
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +21,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path = "/instrument")
 public class InstrumentController
 {
+    @Autowired
+    private InstrumentService instrumentService;
+
     @Autowired
     private InstrumentRepository instrumentRepository;
 
@@ -37,6 +45,15 @@ public class InstrumentController
         SearchResults results = searchService.searchByInstrument(instrumentId);
         response.setSuccess(true);
         response.setResponseData(results);
+        return response;
+    }
+
+    @GetMapping(value = "/getAll")
+    public @ResponseBody ApiResponseObject getBrowse() {
+        ApiResponseObject response = new ApiResponseObject();
+        Set<Instrument> instruments = instrumentService.findAll();
+        response.setSuccess(true);
+        response.setResponseData(instruments);
         return response;
     }
 
